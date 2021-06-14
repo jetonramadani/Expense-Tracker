649efbf593d0ff381ec659f34293ecb11cfb646c
jeton ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-14 23:32:30 +0200

    Dynamic list of expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,7 +4,6 @@ import Card from "../UI/Card";
 import ExpenseFilter from "../FilterExpense/ExpensesFilter";
 import {useState} from "react";
 function Expenses(props) {
-  const expenses = props.expenses;
   const [year, setYear] = useState("2020");
   const yearChangeHandler = (year) => {
     setYear(year);
@@ -12,29 +11,15 @@ function Expenses(props) {
 
   return (
     <div>
-      <h3>Selected year: {year}</h3>
       <Card className="expenses">
         <ExpenseFilter selected={year} onYearChange={yearChangeHandler} />
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+        {props.expenses.map((item) => (
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
       </Card>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,8 +3,8 @@ import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
-      ...enteredExpenseData,
       id: Math.random().toString(),
+      ...enteredExpenseData,
     };
     props.onAddExpense(expenseData);
   };
